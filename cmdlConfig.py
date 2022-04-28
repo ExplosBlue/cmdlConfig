502a611f446fed610df026e6120d2c1d791a8d82
explos <explosblue@gmail.com>
2022-04-28 21:34:00 +0100

    Fix issue preventing materials from being correctly applied from a config

--- cmdlConfig.py
+++ cmdlConfig.py
@@ -7,24 +7,23 @@ class objProperties:
     def __init__(self):
         self.materials = []
 
-    def addMaterial(self, material):
+    def addMaterial(self, material: ElementTree.Element):
         self.materials.append(material)
-
-def createPropertiesList(xmlRoot):
-    propertiesList = objProperties()
-
-    for material in xmlRoot.iter('MaterialCtr'):
-            propertiesList.addMaterial(material)
-
-    return propertiesList
 
 
 def generateConfig():
-    xmlTree = ElementTree.parse(args['infile'])
-    xmlRoot = xmlTree.getroot()
+    cmdlTree = ElementTree.parse(args['infile'])
+    cmdlRoot = cmdlTree.getroot()
 
-    propertyList = createPropertiesList(xmlRoot)
+    propertyList = objProperties()
 
+    # Add Materials to config
+    for material in cmdlRoot.iter('MaterialCtr'):
+        propertyList.addMaterial(material)
+
+    # TODO: Add User Data to config
+
+    # Output the config file
     outPath = ''
 
     if args['outfile'] is None:
@@ -38,38 +37,60 @@ def generateConfig():
     print('Generated config with {matCount} materials'.format(matCount=len(propertyList.materials)))
 
 
+def setMaterialsFromConfig(cmdlRoot: ElementTree.Element, propertiesList: objProperties):
 
-def setMaterialsFromConfig(xmlRoot, propertiesList):
-    materialCount = 0
+    # Get a list of all materials in the CMDL
+    materialsIter = cmdlRoot.iter('MaterialCtr')
+    materialsList = list(materialsIter)
 
-    for models in xmlRoot.iter('Models'):
-        for model in models:
-            for materials in model.iter('Materials'):
+    newMaterials = []
 
-                for material in materials:
+    matChangedCount = 0
+    totalMaterials = len(materialsList)
 
-                    for mat in propertiesList.materials:
+    # Loop over each material in the materials list
+    for material in materialsList:
+        materialExists = False
 
-                        if material.attrib.get('Name') == mat.attrib.get('Name'):
-                            materialCount += 1
+        # Loop over each material in the properties list
+        for mat in propertiesList.materials:
 
-                            materials.remove(material)
-                            materials.append(mat)
+            # Compare material in CMDL with material in properties list
+            if material.attrib.get('Name') == mat.attrib.get('Name'):
 
-    print('Updated {matCount} materials'.format(matCount=materialCount))
+                # If material exists in both the CMDL and the Properties list, store the one from the properties list
+                newMaterials.append(mat)
+                matChangedCount += 1
+                materialExists = True
+                break
+        
+        # Material doesn't exist in our properties list, store the one from the CMDL instead
+        if not materialExists:
+            newMaterials.append(material)
+
+    # Remove all materials from the CMDL and insert the ones from our new materials list
+    for materials in cmdlRoot.iter('Materials'):
+        for material in list(materials):
+
+            for material in list(materials):
+                materials.remove(material)
+
+            for mat in newMaterials:
+                materials.append(mat)
+
+    print('Updated {c} of {t} Materials'.format(c=matChangedCount, t=totalMaterials))
 
 
-def applyConfig(configPath, outPath):
+def applyConfig(configPath: str, outPath: str):
     with open(configPath, 'rb') as infile:
         propertiesList = pickle.load(infile)
-        print(propertiesList)
 
-    xmlTree = ElementTree.parse(args['infile'])
-    xmlRoot = xmlTree.getroot()
+    cmdlTree = ElementTree.parse(args['infile'])
+    cmdlRoot = cmdlTree.getroot()
 
-    setMaterialsFromConfig(xmlRoot, propertiesList)
+    setMaterialsFromConfig(cmdlRoot, propertiesList)
 
-    xmlTree.write(outPath)
+    cmdlTree.write(outPath)
 
 
 if __name__ == "__main__":
